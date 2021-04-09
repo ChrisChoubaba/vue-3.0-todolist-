ee7dc2c5408d73d6548e0d3e69adeaa9060475d2
ChrisChoubaba <52958445+ChrisChoubaba@users.noreply.github.com>
2021-04-09 15:51:11 +0800

    Create List.vue

---
+++ src/components/List.vue
@@ -0,0 +1,55 @@
+<template>
+	<div class="list_wrap">
+		<!-- <input  v-model="inputValue" type="text" /> -->
+
+		<div class="list" v-for="(item, index) in list" :key="item.id">
+			<ListItem :todo="item" @todoDel="todoDel" :todoIndex="index" />
+		</div>
+	</div>
+</template>
+
+<script>
+import { defineComponent, reactive, toRefs } from 'vue'
+import ListItem from './ListItem'
+export default defineComponent({
+	name: 'TodoList',
+	components: {
+		ListItem
+	},
+	props:{
+		list: {
+			type: Array,
+			defalut: 'required'
+		}
+	},
+	setup: (props, context) => {
+		const todoDel = (index) => { // 删去一个项
+			context.emit('todoDel',index)
+		}			
+		return {
+			todoDel
+		}
+	}
+})
+</script>
+
+<style lang="scss" scoped>
+.list_wrap{
+	width: 500px;
+	margin: 0 auto;
+	.list {
+		text-align: start;
+		line-height: 30px;
+		.list_text {
+			display: inline-block;
+			width: 400px;
+		}
+		.clear_button {
+			color: red;
+			background-color: rgb(235, 186, 186);
+			border-color: rgb(235, 186, 186);			
+		}
+	}
+}
+
+</style>
